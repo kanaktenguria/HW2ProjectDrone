f63e3cc8774a4bbfc9a5fb3d5739db31fb8227a5
Kanak Tenguria <kanaktenguria@gmail.com>
2019-09-30 20:33:21 -0600

    Internal position estimation working almost.

--- src/Message/Flip.java
+++ src/Message/Flip.java
@@ -11,11 +11,31 @@ public class Flip extends Message{
     @Override
     public void doAction(DroneCommunicator droneCommunicator,DroneState droneState) throws Exception {
 
+
 //        droneState.move();
         String request= "flip "+ direction;
-
-        droneCommunicator.sendRequest(request);
-        System.out.println(droneCommunicator.receiveRequest()+"received");
+        if(direction.equals("f")|| direction.equals("b")||direction.equals("l")||direction.equals("r")) {
+            droneCommunicator.sendRequest(request);
+            System.out.println(droneCommunicator.receiveRequest() + "received");
+            double xAxis = droneState.getPositionX();
+            double yAxis = droneState.getPositionY();
+            double zAxis = droneState.getPositionZ();
+            if (direction.equals("f")) {
+                yAxis += 10.0;
+                droneState.move(xAxis, yAxis, zAxis);
+            } else if (direction.equals("b")) {
+                yAxis -= 10.0;
+                droneState.move(xAxis, yAxis, zAxis);
+            } else if (direction.equals("l")) {
+                xAxis += 10;
+                droneState.move(xAxis, yAxis, zAxis);
+            } else if (direction.equals("r")) {
+                xAxis -= 10;
+                droneState.move(xAxis, yAxis, zAxis);
+            }
+        }else{
+            System.out.println("Invalid Command.");
+        }
         Thread.sleep(5000);
     }
 
