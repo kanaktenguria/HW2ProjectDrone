a2c8fe367e58fed07e051cf23bebf841717a6aea
Kanak Tenguria <kanaktenguria@gmail.com>
2019-10-04 12:39:22 -0600

    Testing actions Thread creation

--- test/Action/BatteryTest.java
+++ test/Action/BatteryTest.java
@@ -17,14 +17,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class BatteryTest {
     @Test
     public void testDoAction() throws Exception {
-        DroneCommunicator droneCommunicator= new DroneCommunicator("127.0.0.1",9090);
-        DroneState droneState= new DroneState();
-        Status status= new Status(0,0,1,10,10,10,5,50,40,80,29,15.2,56,25.5,25.5,25.5);
-        droneState.setInCommandMode(true);
-        droneState.setHasTakenOff(true);
-        droneState.updateFlyingInfo(status);
-        Battery battery= new Battery();
-        battery.doAction(droneCommunicator,droneState);
+        Thread mockSend= new Thread(new MockSend());
+        Thread mockReply= new Thread(new MockReply());
+        mockReply.start();
+        mockSend.start();
+        //Shows that connection is properly setup and doAction method is working properly. Otherwise it will give some king of exception.
     }
 
     @Test
@@ -33,10 +30,37 @@ class BatteryTest {
         assertNull(battery.getMessageType());
     }
 }
-class MockReply{
+class MockSend implements Runnable{
+
+    @Override
+    public void run() {
+        try {
+            DroneCommunicator droneCommunicator = new DroneCommunicator("127.0.0.1", 9090);
+            DroneState droneState = new DroneState();
+            Status status = new Status(0, 0, 1, 10, 10, 10, 5, 50, 40, 80, 29, 15.2, 56, 25.5, 25.5, 25.5);
+            droneState.setInCommandMode(true);
+            droneState.setHasTakenOff(true);
+            droneState.updateFlyingInfo(status);
+            Battery battery = new Battery();
+            battery.doAction(droneCommunicator, droneState);
+        }catch(Exception E){
+
+        }
+    }
+
+}
+class MockReply implements Runnable{
     String reply=null;
-    public void testMockReply() throws Exception {
-        DroneCommunicator droneCommunicator1= new DroneCommunicator(9090);
-        reply=droneCommunicator1.receiveRequest();
+
+    @Override
+    public void run() {
+        DroneCommunicator droneCommunicator1= null;
+        try {
+            droneCommunicator1 = new DroneCommunicator(9090);
+            reply=droneCommunicator1.receiveRequest();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
--- test/Action/LandTest.java
+++ test/Action/LandTest.java
@@ -1,7 +1,23 @@
 package Action;
 
+import Common.DroneCommunicator;
+import Common.DroneState;
+import Message.Status;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LandTest {
+    @Test
+    public void testDoAction() throws Exception {
+        DroneCommunicator droneCommunicator= new DroneCommunicator("127.0.0.1",9090);
+        DroneState droneState= new DroneState();
+        Status status= new Status(0,0,1,10,10,10,5,50,40,80,29,15.2,56,25.5,25.5,25.5);
+        droneState.setInCommandMode(true);
+        droneState.setHasTakenOff(true);
+        droneState.updateFlyingInfo(status);
+        Land land= new Land();
+        land.doAction(droneCommunicator,droneState);
+    }
 
 }
